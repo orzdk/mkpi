42f7d0030d3f38c3b6b5a40153f581409200cfea
Jesper Christensen <jeschrper@hotmail.com>
2020-07-22 20:06:11 +0200

    Security

---
+++ config-mk.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'database' : 'mongodb://user:pass@xxxxxxxx.mlab.com:nnnnn/dbname'
+}
+
+
--- config-mk.js
+++ config-mk.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'database'	: 'mongodb://mkgeneric:midi1st@ds161520.mlab.com:61520/mkumltest', 
+	'database'	: 'mongodb://mkgeneric:midi1st@ds161520.mlab.com:61520/mkumltest'
 }
 
 
